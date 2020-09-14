c3205114c584b6063667b4d95e685795874fbdff
codwhr <codwhr@163.com>
2020-09-14 12:32:23 +0800

    uuu

--- sml/perceptron.py
+++ sml/perceptron.py
@@ -5,5 +5,21 @@ x = torch.tensor([
     [1,1],
 ])
 y = torch.tensor([
-    1, 1, -1
+    [1], [1], [-1]
 ])
+w = torch.tensor([[0],[0]])
+b = torch.tensor([0])
+
+while True:
+    wx = torch.mm(x, w)
+    loss = -y*(wx+b)
+    loss_flag = loss >= 0
+    if torch.any(loss_flag):
+        for i in range(x.size(0)):
+            if loss_flag[i]:
+                w = w + (y[i]*x[i]).unsqueeze(-1)
+                b = b + y[i]
+                print(w, b)
+                break
+    else:
+        break
